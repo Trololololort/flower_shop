7f1854bf773d7263bfbdc0b89bdeb15477780343
Michael <trololololort@mail.ru>
2023-12-25 13:54:37 +0300

    tmp

--- companies/views.py
+++ companies/views.py
@@ -1,8 +1,15 @@
 from django.views.generic.base import TemplateView
 
+from goods.models import Goods
+
 
 class AboutCompanyView(TemplateView):
     template_name = "companies/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["object_list"] = Goods.objects.all().filter(present=True).order_by("-added")[:5]
+        return context
 
 class ContactsView(TemplateView):
     template_name = "companies/contacts.html"
