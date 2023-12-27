7581208bc657a67f9906d5c1bfda39bc44e18251
Michael <trololololort@mail.ru>
2023-12-28 00:20:00 +0300

    tmp

--- carts/admin.py
+++ carts/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
+from django.contrib import messages
 
 from carts.models import Cart
+
+
+@admin.action(description="Подтвердить заказ")
+def confirm_order(modeladmin, request, queryset):
+    # Админ может выбрать любой из элементов в корзине,
+    # действие будет применено ко всем элементам.
+
+    # https://stackoverflow.com/questions/11764709/can-you-add-parameters-to-django-custom-admin-actions
+
+    unique_uuids = set(queryset.values_list("order_uuid", flat=True).distinct())
+
+    target_elements = Cart.objects.filter(order_uuid__in=unique_uuids)
+    target_elements.update(status="CONFIRMED")
+
+    message = "Подтверждены заказы: {}".format(", ".join([str(elem) for elem in unique_uuids]))
+    messages.info(request, message)
 
 
 class CartAdmin(admin.ModelAdmin):
@@ -8,6 +25,7 @@ class CartAdmin(admin.ModelAdmin):
     list_display = ["id", "user", "order_uuid", "ordered", "status", "goods", "price", "quantity", ]
     list_filter = ["status", "user", "order_uuid", ]
     search_fields = ["order_uuid", ]
+    actions = [confirm_order, ]
 
 
 admin.site.register(Cart, CartAdmin)
--- carts/management/commands/cancel_order.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from carts.models import Cart
-
-# https://stackoverflow.com/questions/11764709/can-you-add-parameters-to-django-custom-admin-actions
-class Command(BaseCommand):
-    pass
