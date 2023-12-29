5a6cf53bab8231b8c68ca0cb50cf5a9b13ffab0e
Michael <trololololort@mail.ru>
2023-12-29 11:08:46 +0300

    tmp

--- accounts/models.py
+++ accounts/models.py
@@ -4,12 +4,16 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    name = models.CharField(max_length=100, blank=False)
-    surname = models.CharField(max_length=100, blank=False)
-    partonymic = models.CharField(max_length=100, blank=False)
-    email = models.EmailField(blank=False)
-    rules = models.BooleanField(blank=False)
-
+    name = models.CharField(max_length=100, blank=False, validators=[
+        cyrillic_validator, ], verbose_name="Имя")
+    surname = models.CharField(max_length=100, blank=False, validators=[
+        cyrillic_validator, ], verbose_name="Фамилия")
+    partonymic = models.CharField(max_length=100, blank=False, validators=[
+        cyrillic_validator, ], verbose_name="Отчество")
+    login = models.CharField(max_length=100, blank=False, validators=[
+        latin_space_hyphen, ], verbose_name="Логин")
+    password = models.CharField(max_length=100, blank=False, validators=[
+        gte_6, ], verbose_name="Пароль")
+    email = models.EmailField(blank=False, verbose_name="Емейл")
+    rules = models.BooleanField(blank=False, null=False, default=False,
+                                verbose_name="Согласен с правилами регистрации")
