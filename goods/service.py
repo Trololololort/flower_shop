7b1bf90696be05c1fa4fe0fad9fd713eaf68a928
Michael <trololololort@mail.ru>
2024-01-06 16:14:58 +0300

    tmp

--- goods/service.py
+++ goods/service.py
@@ -3,6 +3,13 @@ from goods.models import Goods
 
 
 def are_there_enough_goods(user, goods_id):
+    """
+    Нельзя добавить в корзину товаров больше, чем есть в наличии.
+    Пользователь добавляет товар в корзину по одной штуке.
+
+    Проверить, хватает ли товара, если сложить то, что есть в корзине,
+    и 1 шт.
+    """
     goods = Goods.objects.filter(pk=goods_id).first()
     all_goods_in_cart = get_cart_contents(user)
     goods_in_cart = all_goods_in_cart.filter(goods=goods_id).values_list("quantity", flat=True)
